b4f558ce121abcb50d848c8a50c06449e987e094
kiran-jaswant <kiranjaswant08@gmail.com>
2024-05-01 17:11:30 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import mongoose,{model,Schema} from 'mongoose';
+
+dotenv.config();
 
 const app=express();
 app.use(express.json());
 app.use(cors());
 
 const connectDB= async()=>{
-   await mongoose.connect(`mongodb+srv://kiranjaswant:Kiran.2105.Jaswant@icp6.rr5079u.mongodb.net/Notes`)
+   await mongoose.connect(process.env.MONGODB_URL)
     console.log("Connected to DB");
 
 };
